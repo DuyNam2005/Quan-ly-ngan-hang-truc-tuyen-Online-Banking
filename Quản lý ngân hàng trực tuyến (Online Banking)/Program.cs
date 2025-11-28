f53dad25ed6f43988301bb2a1bcac8e5457f78af
DuyNam <duynamnguyen0995@gmail.com>
2025-11-28 16:28:24 +0700

    duy nam push

--- Quản lý ngân hàng trực tuyến (Online Banking)/Program.cs
+++ Quản lý ngân hàng trực tuyến (Online Banking)/Program.cs
@@ -6,12 +6,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    ;
     app.UseSwagger();
     app.UseSwaggerUI();
 }
